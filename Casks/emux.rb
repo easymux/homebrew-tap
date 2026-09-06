5710c71a1c6304dd3f3ab99091fbf94a3bb08e6c
easymux <easymux.dev@proton.me>
2026-09-06 22:57:24 +0200

    chore: emux 0.1.1

--- Casks/emux.rb
+++ Casks/emux.rb
@@ -1,9 +1,9 @@
 cask "emux" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.0"
-  sha256 arm:   "e8dc6cd3043f65207159a6e2b82ac6023b3dc70ecc54969fe132cb172a6493a2",
-         intel: "a8f65b04e9f8976747568f0f4a31347f532fd59d7ab31b6d4dbe076a513c95a4"
+  version "0.1.1"
+  sha256 arm:   "ac119ac93f21c537b0837ddedb99bd94d091edb5e5f17ebf4dd7690867e738b7",
+         intel: "5675e00105fcdda8696b6940d729a02334430599d8e476a39f9811a6143aeec3"
 
   url "https://github.com/easymux/homebrew-tap/releases/download/v#{version}/emux-#{version}-#{arch}.zip"
   name "emux"
